8cd53cfdc7af06c431a41e17b0ddcf7d7c912df4
Rheetham Menon <rheethammenon@gmail.com>
2025-07-27 17:59:20 +0530

    Configured Model mapper

--- shopping-cart/src/main/java/com/rheetham/shoppingcart/controller/ProductController.java
+++ shopping-cart/src/main/java/com/rheetham/shoppingcart/controller/ProductController.java
@@ -142,16 +142,16 @@ public class ProductController {
             return ResponseEntity.ok(new ApiResponse(e.getMessage(), null));
         }
     }
-//
-//    @GetMapping("/product/count/by-brand/and-name")
-//    public ResponseEntity<ApiResponse> countProductsByBrandAndName(@RequestParam String brand, @RequestParam String name) {
-//        try {
-//            var productCount = productService.countProductsByBrandAndName(brand, name);
-//            return ResponseEntity.ok(new ApiResponse("Product count!", productCount));
-//        } catch (Exception e) {
-//            return ResponseEntity.ok(new ApiResponse(e.getMessage(), null));
-//        }
-//    }
+
+    @GetMapping("/product/count/by-brand/and-name")
+    public ResponseEntity<ApiResponse> countProductsByBrandAndName(@RequestParam String brand, @RequestParam String name) {
+        try {
+            var productCount = productService.countProductsByBrandAndName(brand, name);
+            return ResponseEntity.ok(new ApiResponse("Product count!", productCount));
+        } catch (Exception e) {
+            return ResponseEntity.ok(new ApiResponse(e.getMessage(), null));
+        }
+    }
 
 
 }
